c78683f37b0138be35fe5fb0134e06605f1b7ecd
John Cheng <cheng.john@pm.me>
2022-01-25 20:37:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sandbox repo to demo failing plugin with Netlify
+
+## Try it out
+
+1. Clone repository
+2. Run `npm i`
+3. Run `npm run netlify:build`
---
+++ netlify/plugins/netlify-plugin-env-var-by-context/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+	onPreBuild: async () => {
+		// CONTEXT === "production" OR
+		// CONTEXT === deploy-preview OR
+		// CONTEXT === branch-deploy
+		const context = process.env.CONTEXT.toUpperCase().replace(/-/g, '_');
+		Object.keys(process.env).forEach((key) => {
+			const envVar = `${context}_${key}`;
+			const val = process.env[envVar];
+			if (process.env[envVar]) {
+				console.log(`Exporting ${key}=${val}.`);
+				process.env[key] = val;
+			}
+		});
+	}
+};
